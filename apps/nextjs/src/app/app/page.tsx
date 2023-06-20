89be00d6f8e1d3f09eb7d9fae4824af33ffe3d29
David Ilie <david@davidilie.com>
2023-06-20 23:03:26 +0100

    completely functional

--- apps/nextjs/src/app/app/page.tsx
+++ apps/nextjs/src/app/app/page.tsx
@@ -1,21 +1,11 @@
 import type { Metadata } from "next";
 
-import { getServerSession } from "~/lib/getServerSession";
-
-import Logout from "./logout";
-
 export const metadata: Metadata = {
    title: "Home",
 };
 
-const Page = async () => {
-   const session = await getServerSession();
-
-   return (
-      <h1>
-         This is the app, you are logged in as {session?.name}, <Logout />
-      </h1>
-   );
+const Page = () => {
+   return <h1>This is the app</h1>;
 };
 
 export default Page;
