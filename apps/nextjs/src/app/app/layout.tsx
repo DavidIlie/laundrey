ff10db9e37267cbd559452c1cfbbbf15293bc388
David Ilie <david@davidilie.com>
2023-06-21 11:56:49 +0100

    more fixes

--- apps/nextjs/src/app/app/layout.tsx
+++ apps/nextjs/src/app/app/layout.tsx
@@ -15,7 +15,7 @@ export default async function RootLayout({
    return (
       <div className="flex flex-col min-h-screen lg:flex-row">
          <Navigation />
-         <div className="flex-grow px-2 py-4 bg-gray-50 dark:bg-container lg:min-h-screen lg:w-full lg:flex-grow-0 lg:p-6">
+         <div className="flex-grow px-2 py-4 bg-gray-100 dark:bg-container lg:min-h-screen lg:w-full lg:flex-grow-0 lg:p-6">
             {children}
          </div>
       </div>
