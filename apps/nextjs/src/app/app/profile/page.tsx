89469ce1bf8244d1ddb14f33f08aa414cf5f2715
David Ilie <david@davidilie.com>
2023-06-22 23:32:12 +0100

    create empty page

---
+++ apps/nextjs/src/app/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import AppLayout from "~/app/layout/app-layout";
+
+const Page = () => {
+   return (
+      <AppLayout title="Work in Progress">
+         <></>
+      </AppLayout>
+   );
+};
+
+export default Page;
