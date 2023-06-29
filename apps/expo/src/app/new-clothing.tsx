7bf83e537593bfea69eb98e6d49bb34f3a139b8b
David Ilie <david@davidilie.com>
2023-06-29 09:09:55 +0100

    i am a genuis

--- apps/expo/src/app/new-clothing.tsx
+++ apps/expo/src/app/new-clothing.tsx
@@ -1,10 +1,42 @@
 import { Text } from "react-native";
 
-// have the form
-// upload image with expo-camera and store as file object
-// that's it
+import { api } from "~/lib/api";
+
+import { useZodForm } from "@laundrey/api/form";
+import {
+   clientPhotoValidator,
+   clothingValidator,
+} from "@laundrey/api/validators";
+
+import { LoadingOverlayElement } from "~/components/LoadingOverlay";
+
+const joinedValidator = clothingValidator.and(clientPhotoValidator);
 
 const Page = () => {
+   const categoriesR = api.categories.all.useQuery();
+   const brandsR = api.brands.all.useQuery();
+
+   const isLoading = categoriesR.isLoading || brandsR.isLoading;
+
+   const form = useZodForm({
+      schema: joinedValidator,
+      defaultValues: {
+         quantity: 1,
+      },
+   });
+
+   const categories = categoriesR.data?.map((cat) => ({
+      label: cat.name,
+      value: cat.id,
+   }));
+
+   const brands = brandsR.data?.map((cat) => ({
+      label: cat.name,
+      value: cat.id,
+   }));
+
+   if (isLoading) return <LoadingOverlayElement />;
+
    return (
       <>
          <Text>Create a new clothing</Text>
